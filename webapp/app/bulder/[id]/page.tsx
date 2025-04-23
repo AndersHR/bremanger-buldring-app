581c23c5494d16c97901281bcabf5dda26435752
Anders Rokne <33486111+AndersHR@users.noreply.github.com>
2025-04-23 09:39:50 +0200

    Tilbake til gammel henting av isAdmin

--- webapp/app/bulder/[id]/page.tsx
+++ webapp/app/bulder/[id]/page.tsx
@@ -2,12 +2,13 @@ import styles from "@/app/bulder/page.module.css";
 import { BoulderCard } from "@/components/boulder/BoulderCard";
 import SingleBoulderMap from "@/components/kart/SingleBoulderMapWrapper";
 import { fetchBoulderById } from "@/lib/supabase/data.client";
-import { fetchIsAdmin } from "@/lib/supabase/data.server";
+import { createClient } from "@/lib/supabase/serverClient";
 
 export const revalidate = 60;
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { data: isAdmin } = await fetchIsAdmin();
+  const supabase = await createClient();
+  const { data: isAdmin } = await supabase.rpc("is_boulder_admin");
 
   const boulder = await fetchBoulderById(params.id);
 
